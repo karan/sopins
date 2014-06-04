ab50419abb8bc6201d46c82497af2b240faa7bfb
Karan Goel <karan@goel.im>
2014-06-04 14:07:08 -0700

    rearrange params so they make more sense

--- pins.py
+++ pins.py
@@ -14,51 +14,54 @@ from flask import Flask, request, make_response
 app = Flask(__name__)
 
 
+# URLs for getting share counts
 Twitter_URL = "http://urls.api.twitter.com/1/urls/count.json?url=%s"
+
+# subject, status, color, format
 SHIELD_URL = "http://img.shields.io/badge/%s-%s-%s.%s"
 
 
-class TwitterHandler(object):
+class TwitterHandler():
     '''Get the twitter json data for the url, and process.'''
     shield_subject = 'Tweet'
-    format = 'png'
+    shield_color = '55ACEE'
 
-    def get(self, url, format, *args, **kwargs):
-        self.format = format
+    def get(self, url, format):
         url = Twitter_URL % url
         try:
             response = requests.get(url)
             response.raise_for_status()
         except requests.exceptions.HTTPError:
-            return self.write_shield('error', 'red')
+            return self.write_shield('error', 'error', 'red')
         else:
             data = json.loads(response.content)
-            return self.write_shield(data['count'])
+            return write_shield(self.shield_subject, data['count'], 
+                self.shield_color, format)
 
-    def write_shield(self, status, colour='brightgreen'):
-        '''Obtain and write the shield to the response.'''
-        shield_url = SHIELD_URL % (
-            self.shield_subject,
-            status,
-            colour,
-            self.format,
-        )
-        shield_response = requests.get(shield_url)
-        img = BytesIO(shield_response.content)
-        img.seek(0)
-        return img
+def write_shield(subject, count, color, format):
+    '''Obtain and write the shield to the response.'''
+    shield_url = SHIELD_URL % (
+        subject,
+        count,
+        color,
+        format
+    )
+    shield_response = requests.get(shield_url)
+    img = BytesIO(shield_response.content)
+    img.seek(0)
+    return img
 
 
 generators = {
     'twitter': TwitterHandler
 }
 
-@app.route('/<generator>/<extension>/<path:url>', methods=['GET'])
-def shield(generator, extension, url):
+@app.route('/<generator>/<format>/<path:url>', methods=['GET'])
+def shield(generator, url, format='png'):
     gen_class = generators[generator]()
-    img = gen_class.get(url, extension)
+    img = gen_class.get(url, format)
     resp = make_response(img.read())
-    resp.content_type = extension
+    resp.content_type = format
     return resp
 
 
